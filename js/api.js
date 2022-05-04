ce0f643393d9f73e9b757228c172726bdcb17a50
Elvis <elvisbviana@hotmail.com>
2022-05-03 21:12:34 -0300

    Incluindo API externa geradora de frases

---
+++ js/api.js
@@ -0,0 +1,21 @@
+const frase = document.querySelector('#frase');
+const btn = document.querySelector('#botao__frase');
+
+
+btn.addEventListener('click', () => {
+    gerarFrase();
+})
+
+
+function gerarFrase() {
+
+    fetch('https://api.adviceslip.com/advice').then(resposta => {
+        return resposta.json();
+    }).then(fraseData => {
+        const fraseObj = fraseData.slip;
+        frase.innerHTML = `<i>"${fraseObj.advice}"</i>`;
+    }).catch(error => {
+        console.log(error);
+    })
+
+}
